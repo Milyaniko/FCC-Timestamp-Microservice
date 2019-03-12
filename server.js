e4da8230d30ef91e03551f70b163cb77e2a8bd42
Nikolay Milya <Milyaniko@gmail.com>
2019-03-13 00:39:46 +0300

    Routes' logic has been moved to helper. View engine is added.

--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
+const { router } = require('./app')
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-
-// your first API endpoint... 
-app.get("/api/hello", (req, res) => {
-  res.json({greeting: 'hello API'});
-});
-
-
-
-// listen for requests :)
- app.listen(port, () => {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+app.set('view engine', 'ejs');
+app.use('/', router);
+app.listen(port, () => console.log('Your app is listening on port ' + port));
